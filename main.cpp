bcf3309dade35b28b2e358fe58d13169ad674d88
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-28 12:21:46 +0100

    add class

--- main.cpp
+++ main.cpp
@@ -23,8 +23,45 @@
 
 using namespace std;
 
+	// taka klasa może być oczywiście przed main() aby miała zasięg globalny!
+	//{
+		// klasa iteratora, iterująca po liczbach..
+		class num_iterator { // musi ++
+			long long i;
+			bool increase; // rosnie? maleje?
+		public:			
+			// część : i(valueOfi) od razu ustawia this->i = valueOfi itd.
+			num_iterator(long long valueOfi=0, bool inc=true) 
+				: i(valueOfi),increase(inc) {}
+
+			long long operator*() { return i; } // wyłuskanie
+
+			num_iterator& operator++() { 
+				if (increase) ++i; else --i;
+				return *this; 
+			}			
+
+			bool operator!=(num_iterator& other) { return i != other.i; }
+		};
+		class range {
+			long long a;
+			long long b;
+		public:
+			range(long long from, long long to) : a{ from }, b{ to } {}
+			num_iterator begin() { 
+				if (a<b) return num_iterator{ a,true }; 
+				return num_iterator{ a,false };
+			}
+			num_iterator end() { 				
+				if (a<b) return num_iterator{ b + 1, true };
+				return num_iterator{ b - 1, false };
+			} 
+		};
+
 
 int main(){
+
+    
 
     return 0;
 }
